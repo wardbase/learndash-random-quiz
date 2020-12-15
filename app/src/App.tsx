f8e29910c8a56b66d298b82dedcd8e97e6b617a6
KHeo <sainthkh@naver.com>
2020-12-16 03:55:05 +0900

    Use the default CSS.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,7 +3,13 @@ import { QuizApp } from './components/QuizApp';
 import './App.css';
 
 function App() {
-  return <QuizApp />;
+  return (
+    <div className="learndash-wrapper">
+      <div className="wpProQuiz_content">
+        <QuizApp />
+      </div>
+    </div>
+  );
 }
 
 export default App;
